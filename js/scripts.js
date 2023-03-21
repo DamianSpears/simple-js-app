8bc152637e45b18f886b1efcb24251f4ac35bbbf
Damian Spears <ryanspears97@gmail.com>
2023-03-21 11:02:49 -0400

    Added Arrays and Objects
    Added pokemon example to display primitive and complex data types. Removed 'alert' and 'write' command example

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello World');
-
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [
+   {
+      name: 'venusaur',
+      height: 2,
+      type: ['grass', 'poison'],
+   },
+   {
+      name: 'charizard',
+      height: 1.7,
+      type: ['fire', 'flying'],
+   },
+   {
+      name: 'blastoise',
+      height: 1.6,
+      type: 'water',
+   },
+];
