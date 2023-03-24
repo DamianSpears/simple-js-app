d333c94ec8c377c767438901ec69ef113230378e
Damian Spears <ryanspears97@gmail.com>
2023-03-24 11:37:50 -0400

    Created for loop and if else statement
    The for loop uses document.write to display the selected characteristics of the pokemon. That is paired with an if else statement that will identify the pokemon larger than or equal to 1.7m.

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,18 @@ let pokemonList = [
    },
 ];
 
+
+/* Above lists the pokemon inside an array titled "pokemonList". The array contains objects with
+characteristics for each pokemon.*/
+
+let pokemonLarge = ' - wow, that is a large pokemon.';
+
+for (let i=0; i < pokemonList.length; i++) {
+   if (pokemonList[i].height >= 1.7) {
+      document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')' + (pokemonLarge) + ' <br>');}
+   else {
+      document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')<br>');
+   }
+}
+
+/*this for loop has an if...else statement that identifies any pokemon greater than or equal to 1.7m.*/
