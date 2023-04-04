f3869955ccd9ddedd5c11a45e2c78a12d1570e67
Damian Spears <ryanspears97@gmail.com>
2023-04-04 12:14:59 -0700

    Added IIFE
    Put variables into IIFE to make unaccessible globally unless referenced by either the getAll or add key. Added loop referencing the getAll to print the same statement as before

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
-let pokemonList = [
-   {
-      name: 'venusaur',
-      height: 2,
-      type: ['grass', 'poison'],
-   },
-   {
-      name: 'charizard',
-      height: 1.7,
-      type: ['fire', 'flying'],
-   },
-   {
-      name: 'blastoise',
-      height: 1.6,
-      type: 'water',
-   },
-];
+let pokeDex = (function() {
+   let pokemonList = [
+      {
+         name: 'venusaur',
+         height: 2,
+         type: ['grass', 'poison'],
+      },
+      {
+         name: 'charizard',
+         height: 1.7,
+         type: ['fire', 'flying'],
+      },
+      {
+         name: 'blastoise',
+         height: 1.6,
+         type: 'water',
+      },
+   ];
 
-pokemonList.forEach (function(pokemon) {
+   return {
+      add: function(pokemon) {
+         pokeDex.push(pokemon);
+      },
+
+      getAll: function() {
+         return pokemonList;
+      }
+   }
+   //These two keys now allow referencing globally from outside loops//
+   
+})();
+
+pokeDex.getAll().forEach (function(pokemon) {
    document.write (pokemon.name + ', ' + pokemon.height + ', is a ' + pokemon.type + ' type pokemon.<br>')
 });
+//The loop now uses getAll as a function and loops through each object in the array, printing the statement for each pokemon.//
