6de88ff3ebb3bb6b0da056f811b39a46422ecb05
Damian Spears <ryanspears97@gmail.com>
2023-04-20 14:12:37 -0400

    Added function to IIFE
    The addListItem and showDetails functions have been added to the pokemonRepository IIFE to loop through each object of the array and style it and add an event listener.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokeDex = (function() {
+let pokemonRepository = (function () {
    let pokemonList = [
       {
          name: 'venusaur',
@@ -17,20 +17,47 @@ let pokeDex = (function() {
       },
    ];
 
-   return {
-      add: function(pokemon) {
-         pokeDex.push(pokemon);
-      },
-
-      getAll: function() {
-         return pokemonList;
-      }
+   function add(pokemon) {
+     pokemonList.push(pokemon);
    }
-   //These two keys now allow referencing globally from outside loops//
-   
-})();
 
-pokeDex.getAll().forEach (function(pokemon) {
-   document.write (pokemon.name + ', ' + pokemon.height + ', is a ' + pokemon.type + ' type pokemon.<br>')
+   function getAll () {
+      return pokemonList;
+   }
+
+   function addListItem (pokemon) {
+      let pokemonUlist = document.querySelector('.pokemon-list');
+      let listItem = document.createElement('li');
+      let button = document.createElement('button');
+      button.classList.add('pokemon-button');
+      button.innerText = pokemon.name;
+      listItem.appendChild(button);
+      pokemonUlist.appendChild(listItem);
+      button.addEventListener('click', function() {
+         showDetails(pokemon);
+      }
+      );
+   };
+//addListItem currently goes through the array to turn all objects into a button and styles the object.//
+//An event listener has been added to show the details of each pokemon in the console.//
+
+   function showDetails (pokemon) {
+      console.log(pokemon);
+   }
+// This function logs each pokemon object to the console as the loop below goes through each object.//
+   return {
+      getAll: getAll,
+      add: add,
+      addListItem: addListItem,
+      showDetails:showDetails
+      }
+//The above is a series of returns for the defined functions inside the IIFE//
+
+})();
+//Everything above is in an IIFE for 'Pokemon Repository//
+
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+   pokemonRepository.addListItem(pokemon);
 });
-//The loop now uses getAll as a function and loops through each object in the array, printing the statement for each pokemon.//
+//This is a loop that currently adds the objects inside the array as list items and gives them styling through CSS styles//
