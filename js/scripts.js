a73ece83d854bac4231cf16ddb54244d7d0c0b13
Damian Spears <ryanspears97@gmail.com>
2023-04-26 15:01:22 -0400

    Added API and return functions
    Added the Pokedex API to asynchronously update and use new Pokemon. The forEach loop now loops through all 150 items provided by the API

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,24 @@
+fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
+   response.json();
+}).then(function (pokemonList) {
+   console.log(pokemonList);
+}).catch(function (error) {
+   console.error(error);
+});
+
 let pokemonRepository = (function () {
-   let pokemonList = [
-      {
-         name: 'venusaur',
-         height: 2,
-         type: ['grass', 'poison'],
-      },
-      {
-         name: 'charizard',
-         height: 1.7,
-         type: ['fire', 'flying'],
-      },
-      {
-         name: 'blastoise',
-         height: 1.6,
-         type: 'water',
-      },
-   ];
+   let pokemonList = [];
+   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
    function add(pokemon) {
-     pokemonList.push(pokemon);
+      pokemonList.push(pokemon);
    }
 
-   function getAll () {
+   function getAll() {
       return pokemonList;
    }
 
-   function addListItem (pokemon) {
+   function addListItem(pokemon) {
       let pokemonUlist = document.querySelector('.pokemon-list');
       let listItem = document.createElement('li');
       let button = document.createElement('button');
@@ -35,29 +28,69 @@ let pokemonRepository = (function () {
       pokemonUlist.appendChild(listItem);
       button.addEventListener('click', function() {
          showDetails(pokemon);
-      }
-      );
+      });
    };
-//addListItem currently goes through the array to turn all objects into a button and styles the object.//
-//An event listener has been added to show the details of each pokemon in the console.//
+   //addListItem currently goes through the array to turn all objects into a button and styles the object.//
+   //An event listener has been added to show the details of each pokemon in the console.//
 
-   function showDetails (pokemon) {
-      console.log(pokemon);
+   function showDetails(pokemon) {
+         console.log(pokemon);
+   };
+   // This function logs each pokemon object to the console as the loop below goes through each object.//
+
+   function loadList(item) {
+      return fetch(apiUrl).then(function (response) {
+         return response.json();
+      }).then(function (json) {
+         json.results.forEach(function (item) {
+            let pokemon = {
+               name: item.name,
+               detailsUrl: item.Url
+               //detailsUrl will only load JSON, which is why we will create
+               //a new function to parse the JSON (showDetails)
+            };
+            add(pokemon);
+         });
+      }).catch(function (e) {
+         console.error(e);
+      })
    }
-// This function logs each pokemon object to the console as the loop below goes through each object.//
+
+   function loadDetails(item) {
+      let url = item.detailsUrl;
+      return fetch(url).then(function (response) {
+         return response.json();
+      }).then(function (details) {
+         item.imageUrl = details.sprites.front_shiny;
+         item.height = details.height;
+         item.type = details.types;
+      }).catch(function (e) {
+         console.error(e);
+      });
+   }
+   //^This function will take the detailsUrl and parse the JSON code to be used in JS
+
    return {
       getAll: getAll,
       add: add,
       addListItem: addListItem,
-      showDetails:showDetails
-      }
-//The above is a series of returns for the defined functions inside the IIFE//
+      showDetails: showDetails,
+      loadList: loadList,
+      loadDetails: loadDetails,
+   };
+   //The above is a series of returns for the defined functions inside the IIFE//
 
 })();
 //Everything above is in an IIFE for 'Pokemon Repository//
 
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-   pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+   pokemonRepository.getAll().forEach(function (pokemon) {
+      pokemonRepository.addListItem(pokemon);
+   });
 });
-//This is a loop that currently adds the objects inside the array as list items and gives them styling through CSS styles//
+
+//The loadlist function now loads all pokemon from the API before looping through them
+//the promise (.then) is in place to asynchronously load the data from the API
+
+//Within the loadlist there is a loop that currently adds the objects
+//inside the array as list items and gives them styling through CSS styles
