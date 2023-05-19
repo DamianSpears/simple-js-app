869724c04a2e86fd68dbc6001c1d1f231ccf8100
Damian Spears <ryanspears97@gmail.com>
2023-05-19 14:36:04 -0400

    Used bootstrap for reformatting
    Incorporated bootstrap into the index.html in for styling buttons, navbar, and modal. Removed unneeded JS functions and CSS styling that is now incorporated by bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,8 @@ let pokemonRepository = (function () {
       button.classList.add('pokemon-button');
       button.innerText = item.name;
       listItem.appendChild(button);
+      listItem.classList.add('list-group-item')
+      button.classList.add('btn-secondary');
       pokemonUlist.appendChild(listItem);
       button.addEventListener('click', function () {
          showDetails(item);
@@ -42,6 +44,9 @@ let pokemonRepository = (function () {
          console.error(e);
       })
    }
+   //Above is a promise that returns all data from the API url in a promise for asynchronous purposes
+   //once the url data is fetched, the forEach loop goes through and selects only the data described under the "a" variable
+   //which is then added to the pokemonList array via the 'add' function.
 
    function loadDetails(item) {
       let url = item.detailsUrl;
@@ -57,6 +62,7 @@ let pokemonRepository = (function () {
    }
    //^This function will take the detailsUrl and parse the JSON code to be used in JS.
    //Then it will be used in the showDetails function to retrieve pokemon image, height, and type.
+   //the 'name' does not need to be passed through the loadDetails because it is not 
 
    function showDetails(details) {
       loadDetails(details).then(function () {
@@ -66,64 +72,6 @@ let pokemonRepository = (function () {
    // This function is waiting for an input for the details parameter, it should receive 
    //details about height, type, and receive an imageUrl, which will be passed into the showModal function./
 
-   function showModal(details) {
-      pokemonRepository.loadDetails(details).then(function () {
-         //First, this function needs to load the details of the pokemon (imageUrl, height, type)
-         let modalContainer = document.querySelector('#modal-container');
-         modalContainer.innerText = '';
-
-         let modal = document.createElement('div');
-         modal.classList.add('modal');
-
-         let closeButtonElement = document.createElement('button');
-         closeButtonElement.classList.add('modal-close');
-         closeButtonElement.innerText = 'x';
-         closeButtonElement.addEventListener('click', hideModal);
-
-         let pokemonName = document.createElement('h1');
-         pokemonName.innerText = 'name: ' + details.name;
-
-         let pokemonDetails = document.createElement('p');
-         pokemonDetails.innerText = 'Height: ' + details.height;
-
-         let imageContainer = document.createElement('image-container');
-         let pokemonImage = document.createElement('img');
-         pokemonImage.src = details.imageUrl;
-
-         modal.appendChild(closeButtonElement);
-         modal.appendChild(pokemonName);
-         modal.appendChild(pokemonDetails);
-         modal.appendChild(imageContainer);
-         modalContainer.appendChild(modal);
-         imageContainer.append(pokemonImage);
-
-         modalContainer.classList.add('is-visible');
-
-         window.addEventListener('keydown', (e) => {
-            if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-               hideModal();
-            }
-         });
-
-         modalContainer.addEventListener('click', (e) => {
-            // Since this is also triggered when clicking INSIDE the modal container,
-            // We only want to close if the user clicks directly on the overlay
-            let target = e.target;
-            if (target === modalContainer) {
-               hideModal();
-            }
-         });
-         //When this function is called, the is-visible will make the modal appear, as it is initially not visible.
-         //Here, I have created the layout for the modal. When the showModal function is called, the parameter will be
-         //Used to display the pokemone name and height, while the close button will be present to close the modal.
-      })
-   }
-
-   function hideModal() {
-      let modalContainer = document.querySelector('#modal-container');
-      modalContainer.classList.remove('is-visible');
-   }
-
    return {
       getAll: getAll,
       add: add,
@@ -131,7 +79,6 @@ let pokemonRepository = (function () {
       showDetails: showDetails,
       loadList: loadList,
       loadDetails: loadDetails,
-      showModal: showModal
    };
    //The above is a series of returns for the defined functions inside the IIFE//
 
